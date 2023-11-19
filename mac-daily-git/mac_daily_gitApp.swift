7a30c0819f694a5acd3ee2c9074633be2ef4047d
Christian Toledana <toledana.ian@gmail.com>
2023-11-19 23:21:52 +0800

    Initial Commit

---
+++ mac-daily-git/mac_daily_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_daily_gitApp.swift
+//  mac-daily-git
+//
+//  Created by Christian Toledana on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct mac_daily_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
